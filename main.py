b4787b1391bffa108c229cafdc6182cba883ddd7
Amitabh <ranamitabh@gmail.com>
2021-01-31 02:40:49 +0530

    upgradinng calculator

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 # Program to create a calculator  
 
-# Program to Show how to create a switch  
-# import kivy module     
 import kivy
-
-# base Class of your App inherits from the App class.     
-# app:always refers to the instance of your application    
 from kivy.app import App
-
-# this restrict the kivy version i.e   
-# below this kivy version you cannot   
-# use the app or software   
 kivy.require('1.9.0')
-
-# for making multiple bttons to arranging 
-# them we are using this 
 from kivy.uix.gridlayout import GridLayout
-
-# for the size of window 
 from kivy.config import Config
-
-# Setting size to resizable 
+# Setting size to resizable
 Config.set('graphics', 'resizable', 1)
-
-
-## Config.set('graphics', 'width', '400')
-## Config.set('graphics', 'height', '400') 
 
 
 # Creating Layout class 
@@ -35,11 +16,23 @@ class CalcGridLayout(GridLayout):
     def calculate(self, calculation):
         if calculation:
             try:
-                # Solve formula and display it in entry 
-                # which is pointed at by display 
                 self.display.text = str(eval(calculation))
             except Exception:
-                self.display.text = "Error"
+                self.display.text = "Syntax Error"
+
+    def negate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(float(calculation)*-1)
+            except Exception:
+                self.display.text = "Syntax Error"
+
+    def decimal(self, calculation):
+        if "." not in calculation:
+            try:
+                self.display.text = str(calculation+".")
+            except Exception:
+                self.display.text = "Syntax Error"
 
 
 # Creating App class
